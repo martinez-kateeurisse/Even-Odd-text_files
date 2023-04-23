0940149afae28e6a4bc7bcb8eed27af318032dad
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-23 11:46:11 +0800

    Pseudocode

---
+++ Even_Odd_text_files.py
@@ -0,0 +1,19 @@
+#Kate Eurisse L. Martinez_BSCPE 1-5_Even-Odd text files
+
+#A Python program that reads a text file named numbers.txt that contains 20 integers. 
+#The program will create two other text files;the first text file will be named even.txt, 
+#which will contain all even numbers extracted from numbers.txt. 
+#The second text file will be named odd.txt and will contain all odd numbers extracted from numbers.txt.
+
+#Create a text file named numbers.txt 
+#Input 20 integers to the text file
+
+#Open and read the text files
+    #For each line
+        #Convert line to int
+        #If the integer is even
+            #Write in new text file (even.txt)
+        #If the integer is odd
+            #Write in new text file (odd.txt)
+
+#Outputs will be printed in new text files
