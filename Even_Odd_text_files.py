bcf8a9b90b0da9d8157cab9d9ec92b49518ddf03
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-25 21:47:59 +0800

    Program outro added

--- Even_Odd_text_files.py
+++ Even_Odd_text_files.py
@@ -116,3 +116,6 @@ output_label.place(x=50, y=100)
 root.mainloop()
 
 #Output will also be displayed in the tkinter window.(Aside from the txt files)
+
+#Displaying thank you message after the user closes the tkinter window
+prog_design.program_footer()
